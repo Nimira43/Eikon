5108b4e2de685f2d5e310051f2c609f43f31b91b
Nimira43 <n.rabone72@aol.com>
2026-07-16 21:06:57 +0100

    Now able to rotate image.

--- main.py
+++ main.py
@@ -10,21 +10,39 @@ class App(ctk.CTk):
     self.geometry('1000x600')
     self.title('Eikon')
     self.minsize(800, 500)
+    self.init_parameters()
+
     self.rowconfigure(0, weight = 1)
     self.columnconfigure(0, weight = 2, uniform = 'a')
     self.columnconfigure(1, weight = 6, uniform = 'a')
+    
+    self.image_width = 0
+    self.image_height = 0
+    self.canvas_width = 0
+    self.canvas_height = 0
+    
     self.image_import = ImageImport(self, self.import_image)
     self.mainloop()
 
+  def init_parameters(self):
+    self.rotate_float = ctk.DoubleVar(value = ROTATE_DEFAULT)
+    self.rotate_float.trace('w', self.manipulate_image)
+
+  def manipulate_image(self, *args):
+    self.image = self.original
+    self.image = self.image.rotate(self.rotate_float.get())
+    self.place_image()
+
   def import_image(self, path):
-    self.image = Image.open(path)
+    self.original = Image.open(path)
+    self.image = self.original
     self.image_ratio = self.image.size[0] / self.image.size[1]
     self.image_tk = ImageTk.PhotoImage(self.image)
     
     self.image_import.grid_forget()
     self.image_output = ImageOutput(self, self.resize_image)
     self.close_button = CloseOutput(self, self.close_edit)
-    self.menu = Menu(self)
+    self.menu = Menu(self, self.rotate_float)
 
   def close_edit(self):
     self.image_output.grid_forget()
@@ -35,16 +53,22 @@ class App(ctk.CTk):
   def resize_image(self, event):
     canvas_ratio = event.width / event.height
 
-    if canvas_ratio > self.image_ratio:
-      image_height = int(event.height)
-      image_width = int(image_height * self.image_ratio)
-    else:
-      image_width = int(event.width)
-      image_height = int(image_width / self.image_ratio)
+    self.canvas_width = event.width
+    self.canvas_height = event.height
 
+    if canvas_ratio > self.image_ratio:
+      self.image_height = int(event.height)
+      self.image_width = int(self.image_height * self.image_ratio)
+    else:
+      self.image_width = int(event.width)
+      self.image_height = int(self.image_width / self.image_ratio)
+
+    self.place_image()
+
+  def place_image(self):
     self.image_output.delete('all')
-    resized_image = self.image.resize((image_width, image_height))
+    resized_image = self.image.resize((self.image_width, self.image_height))
     self.image_tk = ImageTk.PhotoImage(resized_image)
-    self.image_output.create_image(event.width / 2,  event.height / 2, image = self.image_tk)
+    self.image_output.create_image(self.canvas_width / 2, self.canvas_height / 2, image = self.image_tk)
 
 App()
--- menu.py
+++ menu.py
@@ -2,7 +2,7 @@ import customtkinter as ctk
 from panels import *
 
 class Menu(ctk.CTkTabview):
-  def __init__(self, parent):
+  def __init__(self, parent, rotation):
     super().__init__(master = parent)
     self.grid(row = 0, column = 0, sticky = 'nsew', pady = 10, padx = 10)
 
@@ -11,18 +11,18 @@ class Menu(ctk.CTkTabview):
     self.add('Effects')
     self.add('Export')
 
-    PositionFrame(self.tab('Position'))
+    PositionFrame(self.tab('Position'), rotation)
     ColourFrame(self.tab('Colour'))
 
 class PositionFrame(ctk.CTkFrame):
-  def __init__(self, parent):
+  def __init__(self, parent, rotation):
     super().__init__(master = parent, fg_color = 'transparent')
     self.pack(expand = True, fill = 'both')
 
-    SliderPanel(self, 'Rotation')  
-    SliderPanel(self, 'Zoom')  
+    SliderPanel(self, 'Rotation', rotation, 0, 360)  
+    #SliderPanel(self, 'Zoom')  
 
-class ColourFrame(ctk.CTkFrame):
+class ColourFrame(ctk.CTkFrame): 
   def __init__(self, parent):
     super().__init__(master = parent, fg_color = 'transparent')
     self.pack(expand = True, fill = 'both')
