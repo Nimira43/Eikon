dac3df3eefc3c4c9af2cade3fd140af37fe4fa18
Nimira43 <n.rabone72@aol.com>
2026-07-21 12:25:36 +0100

    Zoom functionality.

--- menu.py
+++ menu.py
@@ -2,7 +2,7 @@ import customtkinter as ctk
 from panels import *
 
 class Menu(ctk.CTkTabview):
-  def __init__(self, parent, rotation):
+  def __init__(self, parent, rotation, zoom):
     super().__init__(master = parent)
     self.grid(row = 0, column = 0, sticky = 'nsew', pady = 10, padx = 10)
 
@@ -11,16 +11,16 @@ class Menu(ctk.CTkTabview):
     self.add('Effects')
     self.add('Export')
 
-    PositionFrame(self.tab('Position'), rotation)
+    PositionFrame(self.tab('Position'), rotation, zoom)
     ColourFrame(self.tab('Colour'))
 
 class PositionFrame(ctk.CTkFrame):
-  def __init__(self, parent, rotation):
+  def __init__(self, parent, rotation, zoom):
     super().__init__(master = parent, fg_color = 'transparent')
     self.pack(expand = True, fill = 'both')
 
     SliderPanel(self, 'Rotation', rotation, 0, 360)  
-    #SliderPanel(self, 'Zoom')  
+    SliderPanel(self, 'Zoom', zoom, 0, 200)  
 
 class ColourFrame(ctk.CTkFrame): 
   def __init__(self, parent):
